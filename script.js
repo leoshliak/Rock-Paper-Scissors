e7097c8f18b3e63c4d4cf05ce423c3ca0a32f274
leoshliak <levshlyak@gmail.com>
2023-09-12 13:37:15 +0300

    final result

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const playerScoreResult = document.querySelector('#player-score');
 const rockBtn = document.querySelector('#rock');
  const paperBtn = document.querySelector('#paper');
  const scissorsBtn = document.querySelector('#scissors');
- const playBtns = document.querySelectorAll('.btn');
- const resultBoard = document.querySelector('#res-board');
 const restartBtn = document.querySelector('#again');
 const res = document.querySelector('#res')
 const wRes = document.querySelector('#w-res');
@@ -15,7 +13,6 @@ let scorePlayer = 0;
 let scoreComputer = 0;
 function getComputerChoice() {
   const choise = option[Math.floor(Math.random() * option.length)];
-  console.log(choise);
   return choise;
 }
  
@@ -60,18 +57,15 @@ rockBtn.addEventListener('click', () => {
 function playRound(playerSelection, computerSelection){
   const result = checkWinner(playerSelection, computerSelection);
   if (result == "Tie") {
-    console.log("It's a Tie!");
     res.innerHTML = "It's a Tie!";
     return "It's a Tie!"
   } 
   else if(result == "Player" &&  scorePlayer < 5 && scoreComputer < 5){
-    console.log( `You Win! ${playerSelection} beats ${computerSelection} `);
     scorePlayer++;
     res.innerHTML = `You Win! ${playerSelection} beats ${computerSelection} `;
     return `You Win! ${playerSelection} beats ${computerSelection} `
   }
   else if(result == "Computer" &&  scorePlayer < 5 && scoreComputer < 5){
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
     scoreComputer++;
     res.innerHTML = `You Lose! ${computerSelection} beats ${playerSelection}`;
     return `You Lose! ${computerSelection} beats ${playerSelection}`
@@ -80,16 +74,13 @@ function playRound(playerSelection, computerSelection){
  function checkForWinner(scorePlayer, scoreComputer){
   computerScoreResult.innerHTML = `${scoreComputer}`;
   playerScoreResult.innerHTML = ` ${scorePlayer}`;
- console.log(`Player score: ${scorePlayer} Computer score: ${scoreComputer} `);
  if(scorePlayer >= 5){
-  console.log("You Won the game!!!");
   res.innerHTML = null;
   wRes.innerHTML = "You Won the game!!!";
   lRes.innerHTML = null;
   return "You Won the game!!!!"
  }
  else if(scoreComputer >= 5){
-  console.log("You Lost the game :(");
   res.innerHTML = null;
   lRes.innerHTML = "You Lost the game :(";
   wRes.innerHTML = null;
